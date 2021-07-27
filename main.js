67472ec5cd9698e2db168f2dd6eac03d3d738d62
JStan811 <jakestandley95@gmail.com>
2021-07-27 12:40:45 -0500

    plenty of changes: completed assignment / program now works as intended, added minor style changes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-console.log('Welcome. You have entered into a Rock Paper Scissors battle. Your objective is to defeat the computer in a best of 5 match. Good luck.')
+const divResults = document.querySelector('#results');
+const divScore = document.querySelector('#score');
+const divFinal = document.querySelector('#final');
+const divReset = document.querySelector('#resetButton');
 
 // function that randomly generates either 0, 1, or 2
 function getRandomNum() {
@@ -25,7 +28,8 @@ function computerPlay() {
 
 // function that plays one round, taking computer's play and player's play then deciding and declaring the winner.
 function playRound(playerPlay, computerPlay) {
-    let roundStatus = ''
+    let roundStatus = '';
+
     // make playerPlay argument all lower-case letters so it is case-insensitive
     playerPlay = playerPlay.toLowerCase();
     // if computer wins, return 'Round lost!'. If player wins, return 'Round won!'
@@ -35,54 +39,19 @@ function playRound(playerPlay, computerPlay) {
     else {
         roundStatus = 'Round won!';
     }
-    console.log(roundStatus);
+
+    divResults.textContent = roundStatus;
 }
-
-/* - one function that represents one full game that plays 5 rounds and reports the winner
-    - this should console.log() the result of each round and the winner at the end */
-
-// function that plays 5 rounds then reports the winner
-function game() {
-    // run playRound 5 times, keep track of score, then calculate and declare winner
-    playerCount = 0;
-    computerCount = 0;
-
-    // function that prompts user for play, then runs playRound using user's input. If user wins, increment playerCount by 1 and return round status message (victory or defeat) and total score. If user loses, increment computerCount by 1
-    function roundMessageAndScore() {
-        let playerPlay = prompt("Enter your play: ");
-        let roundStatus = playRound(playerPlay, computerPlay());
-        if (roundStatus === 'Round won!') {
-            playerCount++;
-        }
-        else {
-         computerCount++;
-        }
-        console.log(roundStatus + '\nScore: Player ' + playerCount + ' | ' + 'Computer ' + computerCount);
-    }
-
-    //run roundMessageAndScore 5 times for 5 rounds
-    // roundMessageAndScore();
-    // roundMessageAndScore();
-    // roundMessageAndScore();
-    // roundMessageAndScore();
-    // roundMessageAndScore();
-   
-    //tally total score and declare winner
-    if (playerCount > computerCount) {
-        console.log('Victory! You are the champion.');
-    }
-    else {
-        console.log('You have been defeated. Keep training.');
-    }
-}
-
-game();
 
 //Below this comment is the code for adding a UI re: The Odin Project section DOM manipulation
 //function that runs playRound using the id from the click event, ie it sets the player selection based on which button is clicked
+let playerScore = 0;
+let computerScore = 0;
+
 function playRoundOnClick(e) {
     let playerSelection = e.srcElement.id;
     playRound(playerSelection, computerPlay());
+    keepScore();
 }
 
 //create node list reference to all buttons
@@ -90,3 +59,39 @@ const butts = document.querySelectorAll('button');
 
 //runs playRoundOnClick every time a button is clicked
 butts.forEach(butt => butt.addEventListener('click', playRoundOnClick)); 
+
+function keepScore() {
+    if(divResults.innerText === 'Round won!') {
+        playerScore++;
+    }
+    else if(divResults.innerText === 'Round lost!') {
+        computerScore++;
+    }
+    divScore.textContent = 'Score: ' + playerScore + ' - ' + computerScore + '\r\n\r\nChoose your next play.';
+
+    if (playerScore === 3) {
+        divScore.textContent = 'Score: ' + playerScore + ' - ' + computerScore;
+        divFinal.textContent = 'Victory! You are the champion.';
+        butts.forEach(butt => butt.remove());
+        const resetButton = document.createElement('button');
+        resetButton.innerHTML = 'Reset';
+        divReset.appendChild(resetButton);
+    }
+
+    if (computerScore === 3) {
+        divScore.textContent = 'Score: ' + playerScore + ' - ' + computerScore;
+        divFinal.textContent = 'You have been defeated. Keep training.';
+        butts.forEach(butt => butt.remove());
+        const resetButton = document.createElement('button');
+        resetButton.innerHTML = 'Reset';
+        divReset.appendChild(resetButton);
+    }
+};
+
+//function to refresh page
+function refreshPage() {
+    location.reload();
+}
+
+//add listener to refresh page when reset button is clicked
+resetButton.addEventListener('click', refreshPage);
